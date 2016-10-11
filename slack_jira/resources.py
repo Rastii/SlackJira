351152d97bcaa4915b7cf1471f4da731e756ff97
rastii <rastii09@gmail.com>
2016-10-10 23:46:52 -0700

    Initial commit: Added resource to extract summary JIRA information

---
+++ slack_jira/resources.py
@@ -0,0 +1,281 @@
+import ConfigParser
+import jira
+import logging
+import collections
+import re
+
+logger = logging.getLogger(__name__)
+
+
+class Error(Exception):
+    """
+    Module level error
+    """
+
+
+class ConfigError(Error):
+    """
+    There was an error loading/parsing the configuration
+    """
+
+
+class JiraError(Error):
+    """
+    There was an issue instantiating the JIRA object.
+    """
+
+
+def get_config_value(config_parser, section, option):
+    """
+    Retrieves a config value from a config parser.
+
+    :type config_parser: ConfigParser.ConfigParser
+    :param config_parser: The config parser object instance to extract values from
+    :type section: str
+    :param section: The section to extract values from
+    :type option: str
+    :param option: The option to extract a value from
+
+    :rtype: str
+    :return: The value extracted from the ConfigParser object
+
+    :raises ConfigError: When the ConfigParser did not contain the specified value
+        based on the section and option
+    """
+    try:
+        value = config_parser.get(section, option)
+    except ConfigParser.Error:
+        raise ConfigError("Missing configuration '{}' in section '{}'".format(option, section))
+
+    return value
+
+
+__ISSUE_SUMMARY_FIELDS = [
+    "issue",
+    "title",
+    "status",
+    "priority",
+    "description",
+    "link",
+    "assignee",
+    "original_estimate",
+    "remaining_estimate",
+]
+
+
+class JiraIssueSummary(collections.namedtuple("JiraIssueSummary", __ISSUE_SUMMARY_FIELDS)):
+    """
+    Named tuple that contains a summary of a Jira issue.
+    """
+    __slots__ = ()
+
+
+class SlackJira(object):
+    """
+    Object stores an authenticated JIRA instance and provides methods
+    that are useful in retrieving summary information of JIRA issues.
+    """
+    # Default section to parse information from
+    OAUTH_SECTION = "oauth"
+    SERVER_SECTION = "server"
+
+    JIRA_TICKET_RE_STR = "[A-Z]{1,10}-[0-9]+"
+    JIRA_TICKET_RE = re.compile(JIRA_TICKET_RE_STR, re.IGNORECASE)
+
+    # Required fields in config parser
+    # TODO: Make this work with other methods other than OAUTH
+    SERVER_FIELDS = (
+        "server"
+    )
+    OAUTH_FIELDS = (
+        "access_token",
+        "access_token_secret",
+        "consumer_key",
+        "key_cert"
+    )
+
+    def __init__(self, authed_jira):
+        """
+        :type authed_jira: jira.JIRA
+        :param authed_jira: An authenticated jira.JIRA object that will be used
+            to obtain information about various JIRA issues.
+        """
+
+        # Store the authenticated jira instance for future queries
+        self.__jira = authed_jira
+        # Store all known projects so we only query issues in known projects
+        self.__projects_lookup = self.get_project_lookup()
+
+    @property
+    def jira(self):
+        """
+        :rtype: jira.JIRA
+        :return: Property provides direct access to the authenticated JIRA object
+        """
+        return self.__jira
+
+    def is_project(self, project):
+        """
+        Does the specified project exist
+
+        :type project: str
+        :param project: The project key as defined in JIRA, i.e, if the issue is
+            ISSUE-1337 then the project would be "ISSUE"
+
+        :rtype: bool
+        :return: Boolean signifying if the project was found
+        """
+        return self.__projects_lookup.get(project, False)
+
+    def get_project_lookup(self):
+        return {getattr(k, "key"): True for k in self.__jira.projects()}
+
+    def get_projects(self, refresh=False):
+        """
+        Get a list of all the available projects.
+
+        :type refresh: bool
+        :param refresh: If True, returns a non cached copy of the projects and will
+            store the new project lookup dict.
+
+        :rtype: list
+        :return: Returns a list of all the known projects in the JIRA server
+        """
+        if refresh:
+            self.__projects_lookup = self.get_project_lookup()
+
+        return self.__projects_lookup.keys()
+
+    def __get_attr_helper(self, object, field, default=None):
+        """
+        Helper method is needed to call hasattr first and then calling getattr
+        with a default value.
+
+        The __getattr__ method is supposed to handle a default case, but it
+        seems like it is not written properly (yet) :-(
+        """
+        # TODO: Make PR to fix this ^ bug
+        if hasattr(object, field):
+            return getattr(object, field)
+
+        return default
+
+    def get_summary(self, issue):
+        """
+        Get the general summary of a JIRA issue.
+
+        :type issue: str
+        :param issue: The issue key in JIRA.  This is typically a value that contains
+            a string, then a hyphen, then numbers such as "ISSUE-1337".
+
+        :rtype: JiraIssueSummary|None
+        :return: A JiraIssueSummary namedtuple (if the issue was valid and found) otherwise
+            None is returned
+        """
+        # Ensure that we do have a valid issue
+        if not self.JIRA_TICKET_RE.match(issue):
+            return logger.warning("Attempted to retrieve invalid ticket: %s", issue)
+
+        project, number = issue.split("-")
+        project = project.upper()
+
+        # Ensure that we only attempt to retrieve valid issues
+        if not self.is_project(project):
+            return logger.warning("Attempted to retrieve invalid ticket: %s", issue)
+
+        try:
+            result = self.__jira.issue(
+                issue,
+                fields=[
+                    "summary",
+                    "description",
+                    "priority",
+                    "status",
+                    "timetracking",
+                    "assignee",
+                ],
+            )
+        except jira.JIRAError as e:
+            logger.error("Error loading issue {}: {}".format(issue, e))
+            return None
+
+        assignee = None
+        if result.fields.assignee:
+            assignee = self.__get_attr_helper(result.fields.assignee, "displayName")
+
+        return JiraIssueSummary(
+            issue=issue,
+            title=result.fields.summary,
+            status=result.fields.status,
+            priority=result.fields.priority,
+            description=result.fields.description,
+            link=result.permalink(),
+            assignee=assignee,
+            original_estimate=self.__get_attr_helper(
+                result.fields.timetracking, "originalEstimate"
+            ),
+            remaining_estimate=self.__get_attr_helper(
+                result.fields.timetracking, "remainingEstimate"
+            )
+        )
+
+    @staticmethod
+    def from_config(conf, oauth_section=OAUTH_SECTION, server_section=SERVER_SECTION,
+                    newline_encoding="|"):
+        """
+        Instantiates a JiraSlack object from a ConfigParser object.
+
+        This includes parsing out OAUTH credentials and the server location AND
+        authenticating to the server with the provided credentials.  The authenticated
+        JIRA object is then injected into the SlackJira object.
+
+        NOTE: Due to the way INI files work, the key_cert value must NOT contain any
+              newline characters.  Instead, please "encode" the newline characters as
+              a different (non base64 character).  The default for this is a "|" (PIPE)
+              character.  Ultimately, this can be defined as the `newline_encoding` param
+
+        There needs to be two sections in the config file, the first section contains
+        server information such as the following:
+
+        [server]
+        server = <ip_of_server>
+
+        The second section needs the oauth credentials, such as the following:
+
+        [oauth]
+        access_token = The OAUTH access token (obtained by doing the OAUTH dance)
+        access_token_secret = The OAUTH access token secret (obtained by doing the OAUTH dance)
+        consumer_key = The JIRA consumer key as defined in JIRA
+        key_cert = The private key of the key pair used to configure in JIRA
+
+        :type conf: ConfigParser.ConfigParser
+        :param conf: The config object to parse settings from
+        :type oauth_section: str
+        :param oauth_section: The section from the config that contains
+            the oauth information (optional)
+        :type server_section: str
+        :param server_section: The section from the config that contains
+            the server information (optional)
+        :type newline_encoding: str
+        :param newline_encoding: The character that replaces a newline character.
+            When obtaining the key_cert, this value will be replaces with newlines.
+
+        :rtype: SlackJira
+        :return: An instantiated SlackJira from the config parser.
+
+        :raises JiraError: If there was an error instantiated the JIRA object
+        :raises ConfigError: If there was an error parsing the config file
+            (perhaps a configuration was missing)
+        """
+
+        oauth_dict = {k: get_config_value(conf, oauth_section, k) for k in SlackJira.OAUTH_FIELDS}
+        oauth_dict["key_cert"] = oauth_dict["key_cert"].replace(newline_encoding, "\n")
+        server_location = get_config_value(conf, server_section, "server")
+
+        try:
+            authed_jira = jira.JIRA(server=server_location, oauth=oauth_dict)
+            lljira = SlackJira(authed_jira)
+
+            return lljira
+        except jira.JIRAError as e:
+            raise JiraError(e)
